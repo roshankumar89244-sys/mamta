cd6e29da1b8920defba45a28728ab90a095b8978
roshankumar89244-sys <roshankumar89244@gmail.com>
2026-06-25 18:42:15 +0530

    swap with 3rd veriable

---
+++ swapin3.py
@@ -0,0 +1,9 @@
+# swaping 2 no using 3rd variable
+print("Enter 2 numbers")
+a = int(input())
+b = int(input())
+print("Before swapping a:", a, "b:", b)
+temp = a
+a = b
+b = temp
+print("After swapping a:", a, "b:", b)
