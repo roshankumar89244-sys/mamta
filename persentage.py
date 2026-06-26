a64ef2db24fecf1c3d43fd2f509af10b5f2a1c97
roshankumar89244-sys <roshankumar89244@gmail.com>
2026-06-26 14:39:13 +0530

    pertenge

---
+++ persentage.py
@@ -0,0 +1,5 @@
+# total and percentage of students marks
+total = int(input("Enter total: "))
+marks = int(input("Enter marks: "))
+percentage = (marks / total) * 100  
+print("Percentage is:", percentage)
